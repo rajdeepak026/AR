224a6373f08a3d6bb47d35e6a74d2674bfe50448
Raj Deepak <rajdeepak02004@gmail.com>
2025-07-01 23:33:36 +0530

    fix-02

--- backend/controllers.py
+++ backend/controllers.py
@@ -16,11 +16,15 @@ def send_push_notification(player_id, heading, content):
         "Authorization": "Basic kus37ni6auldnwy3jruygbw7h"
     }
     payload = {
-        "app_id": "b247bbe3-988e-4438-b5b2-74207755fea4",
-        "include_player_ids": [player_id],
-        "headings": {"en": heading},
-        "contents": {"en": content}
+    "app_id": "b247bbe3-988e-4438-b5b2-74207755fea4",
+    "include_player_ids": [player_id],
+    "headings": {"en": heading},
+    "contents": {"en": content},
+    "chrome_web_icon": "https://img.icons8.com/emoji/48/bell-emoji.png",
+    "ttl": 60,
+    "priority": 10
     }
+
     try:
         response = requests.post(url, json=payload, headers=headers)
         print("Push status:", response.status_code)
